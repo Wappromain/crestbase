0a2a81cf621fb8f2679190ca47b47dc735a11cc3
Patrick Wande <ayomikunwande@gmail.com>
2023-07-01 16:31:35 +0100

    first commit

---
+++ src/components/HeroSection/HeroSection.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const HeroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const HeroTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+`;
+
+const HeroMainText = styled.p``;
---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <>
+      <div>This is the Hero Section</div>
+    </>
+  );
+};
+
+export default HeroSection;
