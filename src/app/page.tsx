026f8034e85d8362c33c5abeda4386c1565bfbab
Patrick Wande <ayomikunwande@gmail.com>
2023-07-11 21:51:08 +0100

    Mobile view for How it Works Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import HeroSection from '@/components/HeroSection';
 import MobileHeader from '@/components/Header/MobileHeader';
+import HowItWorks from '@/components/HowItWorks';
 
 export default function Home() {
   return (
     <>
       <MobileHeader />
       <HeroSection />
+      <HowItWorks />
     </>
   );
 }
